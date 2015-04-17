27a478f4e5fbfd12f39e8e0d337672a36a5c25f9
salexzee <samwebb@codingtestsite.com>
2015-04-18 00:44:40 +0930

    Add a few pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,14 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new; end
+
+  def create; end
+
+  def edit;  end
+
+  def update; end
+
+  def destroy; end
+
 end
