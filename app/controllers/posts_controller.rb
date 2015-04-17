82528196eadb49dd14cbebe01e4f82efb7914196
Benji Dalton <daylightsavings@gmail.com>
2015-04-18 01:18:33 +1200

    complets post index and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,11 @@
 class PostsController < ApplicationController
-  
+
   def index
     @posts = Post.all
-    respond_to @posts
   end
-  
+
   def show
     @post = Post.find(params[:id])
-    respond_to @post
   end
-  
+
 end
