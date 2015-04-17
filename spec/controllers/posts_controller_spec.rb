866324d60cde72236c7eb08c73c424df44b1146c
Benji Dalton <daylightsavings@gmail.com>
2015-04-18 01:58:21 +1200

    fixes tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,20 +3,21 @@ require 'rails_helper'
 RSpec.describe PostsController, type: :controller do
   it "lists an index of posts" do
     10.times { post = create(:post) }
-    
+
     get :index
     posts_present = Post.count
-    
+
     expect(response).to have_http_status(:success)
     expect(posts_present).to eq(10)
   end
-  
-  xit "returns a specific post" do
+
+  it "returns a specific post" do
     post = create(:post, title: "a very specific post")
 
     get :show, id: post.id
-    post_returned = response.body
+    post_returned = Post.find(post.id)
 
+    expect(response).to render_template(:show)
     expect(response).to have_http_status(:success)
     expect(post_returned.title).to eq("a very specific post")
   end
