d79d716308029344f2cef5b287bf07cda1135229
Benji Dalton <daylightsavings@gmail.com>
2015-04-26 05:10:46 +1200

    disables confirmable for sign-up for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
-
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
 
   def author?
     role == 'author'
