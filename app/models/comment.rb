3a82eee64dadf4576ee46b13d1052958bd889493
Benji Dalton <daylightsavings@gmail.com>
2015-05-05 20:38:25 +1200

    changes minmum comment validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ActiveRecord::Base
 	belongs_to :post
   belongs_to :user
   validates :body, presence: true,
-               length: { minimum: 20}
+               length: { minimum: 2}
 end
