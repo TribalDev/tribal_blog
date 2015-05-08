fd1c1b4e77c40df0326705d92a60527a04b9bb9c
Benji Dalton <daylightsavings@gmail.com>
2015-05-09 01:00:54 +1200

    adds category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424135539) do
+ActiveRecord::Schema.define(version: 20150508125957) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
