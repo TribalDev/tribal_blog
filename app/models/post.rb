aeda4a110dce7bf3e138c19c588fb04fd358a68d
Benji Dalton <daylightsavings@gmail.com>
2015-05-09 01:07:48 +1200

    adds category-post association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  belongs_to :category
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150508125957) do
+ActiveRecord::Schema.define(version: 20150508130233) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -32,11 +32,13 @@ ActiveRecord::Schema.define(version: 20150508125957) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.integer  "category_id"
   end
 
+  add_index "posts", ["category_id"], name: "index_posts_on_category_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
