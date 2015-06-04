2a58a8d6d4cf68d306f4a4cc7e36effc66d7e310
Benji Dalton <daylightsavings@gmail.com>
2015-06-05 02:26:42 +1200

    fixing assets continued

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ Bundler.require(*Rails.groups)
 
 module Blog
   class Application < Rails::Application
+    config.serve_static_assets = true
+    config.assets.precompile += %w( tinymce/plugins/uploadimage/plugin.js tinymce/plugins/uploadimage/langs/en.js )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
